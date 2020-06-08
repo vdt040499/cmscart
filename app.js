f695784c96a5cce5669bf3896bbf35c1b5342125
VoTan <vdt040499@gmail.com>
2020-06-08 19:56:07 +0700

    Connect to MongoDB

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Connect to MongoDB
+mongoose.connect(config.database);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection errors'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');

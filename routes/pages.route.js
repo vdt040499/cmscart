564784853e5f0f10d1dc23b4668dc4cbd375a8d1
VoTan <vdt040499@gmail.com>
2020-06-11 13:28:18 +0700

    Display page content

--- routes/pages.route.js
+++ routes/pages.route.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const Page = require('../models/page.model');
+
 router.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Home'
+    Page.findOne({slug: "home"}, function(err, page) {
+        if(err) console.log(err);
+    
+        res.render('index', {
+            title: page.title,
+            content: page.content
+        });
     });
 })
+
+//GET a page
+router.get('/:slug', (req, res) => {
+    var slug = req.params.slug;
+
+    Page.findOne({slug: slug}, function(err, page) {
+        if(err) console.log(err);
+        
+        if(!page) {
+            res.redirect('/');
+        } else {
+            res.render('index', {
+                title: page.title,
+                content: page.content
+            });
+        }
+    });
+});
 
 module.exports = router;

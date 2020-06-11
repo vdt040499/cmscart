e0eac67b1670cc4c8a765f420d341c52893f4916
VoTan <vdt040499@gmail.com>
2020-06-11 19:31:24 +0700

    Display products by category

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const Product = require('../models/product.model');
+const Category = require('../models/category.model');
 
+//Get all products
 router.get('/', function(req, res) {
     Product.find((err, products) => {
         if(err) {
@@ -14,6 +16,23 @@ router.get('/', function(req, res) {
             })
         }
     })
+});
+
+//Get products by category
+router.get('/:category', (req, res) => {
+
+    var categorySlug = req.params.category;
+
+    Category.findOne({slug: categorySlug}, (err, cat) => {
+        Product.find({category: categorySlug}, (err, products) => {
+            if(err) console.log(err);
+
+            res.render('product/cat_products', {
+                title: cat.title,
+                products: products
+            });
+        });
+    });
 });
 
 module.exports = router;

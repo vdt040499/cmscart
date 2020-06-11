93b7730a68e297afc25f61984b0f405aea7316ae
VoTan <vdt040499@gmail.com>
2020-06-11 20:39:37 +0700

    Display product details

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,5 +11,9 @@ $(function () {
     $('a.confirmDeletion').on('click', function() {
         if(!confirm('Confirm deletion')) return false; 
     });
+
+    if ($('[data-fancybox]').length) {
+      $('[data-fancybox]').fancybox();
+    }
   });
 
--- routes/products.route.js
+++ routes/products.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs-extra');
 
 const Product = require('../models/product.model');
 const Category = require('../models/category.model');
@@ -32,6 +33,34 @@ router.get('/:category', (req, res) => {
                 products: products
             });
         });
+    });
+});
+
+//Get product details
+router.get('/:category/:product', (req, res) => {
+    
+    var galleryImages = null;
+
+    Product.findOne({slug: req.params.product}, (err, product) => {
+        if(err) {
+            console.log(err);
+        } else {
+            var galleryDir = 'public/product_images/' + product._id + '/gallery';
+
+            fs.readdir(galleryDir, (err, files) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    galleryImages = files;
+
+                    res.render('product/product_detail', {
+                        title: product.title,
+                        p: product,
+                        galleryImages: galleryImages
+                    });
+                }
+            })
+        }
     });
 });
 
